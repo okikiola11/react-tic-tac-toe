dfff113d66164a523c65680647fb80d5c57c0c28
okikiola11 <okikiola.apelehin@gmail.com>
2021-05-04 13:30:33 +0000

    Add a function as onClick prop to Square.js

--- src/Square.js
+++ src/Square.js
@@ -3,7 +3,7 @@ import React from "react";
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('click') }>
         {this.props.value}
       </button>
     );
