43a011b837a3afffcfd19d3a8bd02f7b31a246c3
okikiola11 <okikiola.apelehin@gmail.com>
2021-05-04 11:45:03 +0000

    Add the Board module class Add the Game class to the index.js class

--- src/Board.js
+++ src/Board.js
@@ -12,9 +12,24 @@ class Board extends React.Component {
     return (
       <div>
         <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     );
   }
 }
 
-export default Board
+export default Board;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Game from './Game';
 import './index.css';
 
-// ReactDOM.render(
-//     <App />,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+    <Game />,
+  document.getElementById('root')
+);
